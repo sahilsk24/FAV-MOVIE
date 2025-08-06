7551301c9e4391ca0e1367344c24a86cba43b117
Sahil Shaik <sahilshaik0809@gmail.com>
2025-08-06 12:47:08 +0530

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
